9e984d7fd9c3e2ecbf4c39698bc311e59d3ad9c5
Hannah <hannahsenft@gmx.net>
2022-03-26 19:36:44 +0100

    add custom 404 page with font-awesome icon

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,4 @@
+import { FaExclamationTriangle } from "react-icons/fa"; //fa is font-awesome
 import Link from "next/link";
 import Layout from "../components/Layout";
 import styles from "../styles/404.module.css";
@@ -6,9 +7,11 @@ export default function NotFoundPage() {
     return (
         <Layout title="Page Not Found">
             <div className={styles.error}>
-                <h1>404</h1>
+                <h1>
+                    <FaExclamationTriangle /> 404
+                </h1>
                 <h4>Sorry, there is nothing here</h4>
-                <Link href='/'>Go Back Home</Link>
+                <Link href="/">Go Back Home</Link>
             </div>
         </Layout>
     );
